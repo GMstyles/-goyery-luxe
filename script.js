38967a70cd2d6bcb94f60cd45025a3a65e557be7
PG.SAS <jeffersongarcia2001@hotmail.com>
2025-06-09 05:54:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeEls = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  fadeEls.forEach(el => observer.observe(el));
+});
