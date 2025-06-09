d74052b4261072a7cb653d25f8699a6ab491ac8f
PG.SAS <jeffersongarcia2001@hotmail.com>
2025-06-09 05:32:53 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function enviarWhatsApp(imagen) {
-  const numero = "573155588648";
-  const mensaje = "Hola, estoy interesado en esta joya: " + window.location.href.replace("index.html", "") + imagen;
-  const url = https://wa.me/${numero}?text=${encodeURIComponent(mensaje)};
-  window.open(url, "_blank");
-}
