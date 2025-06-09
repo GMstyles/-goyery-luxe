e092ceb50268b274dbe920ba691973f3d9bf2a1a
PG.SAS <jeffersongarcia2001@hotmail.com>
2025-06-09 04:06:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// JavaScript básico (si deseas agregar funciones dinámicas)
+function enviarWhatsApp(imagen) {
+  const numero = "573155588648";
+  const mensaje = "Hola, estoy interesado en esta joya: " + window.location.href.replace("index.html", "") + imagen;
+  const url = https://wa.me/${numero}?text=${encodeURIComponent(mensaje)};
+  window.open(url, "_blank");
+}
